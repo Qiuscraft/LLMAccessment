818c7ab2f75aead9142788582c7869bee65ebdd7
Qiu Jun <qiujun2004@outlook.com>
2025-05-26 01:06:59 +0800

    feat: 原始问题列表页面

---
+++ app/pages/original-question-list.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ButtonNewOriginalQuestion from "~/components/buttons/ButtonNewOriginalQuestion.vue";
+
+definePageMeta({
+  layout: 'default',
+});
+</script>
+
+<template>
+  <ButtonNewOriginalQuestion></ButtonNewOriginalQuestion>
+</template>
+
+<style scoped>
+
+</style>
+
